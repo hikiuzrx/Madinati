0edf1f63b8dd437e8a27c930dfc47992abf7a6b3
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-02-23 23:39:47 +0100

    kje

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='relative flex flex-col items-center justify-center lg:min-h-screen gap-8 p-4 pb-16'>
+    <div className='relative flex flex-col items-center justify-center lg:min-h-screen gap-8 p-4 pb-16 w-full'>
      <div>
             <Navigation />
             </div>
