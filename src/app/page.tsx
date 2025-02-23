49d8d08928897902ca7320764a9a78a4704c6ce3
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-02-23 23:11:55 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,20 @@ const Report = dynamic(() => import("@/components/Report"), { ssr: false });
 export default function Home() {
     return (
         <>
-            <Badges>
-                <Report />
-                <Warning />
-            </Badges>
+           
             <div className="relative flex flex-col items-center justify-center lg:min-h-screen gap-8 p-4 pb-16">
                 <div className="lg:w-[80%] mx-auto px-4 mt-6">
                     <h1 className="text-3xl font-bold text-primary drop-shadow-sm">
-                        Madinati Demo 🤖 🇩🇿
+                      madianti
                     </h1>
                 </div>
-                <a
-                    href="/#article"
-                    className="absolute bottom-6 right-8 items-center justify-center shadow-sm transition-all hidden md:flex text-3xl hover:scale-125 hover:-translate-y-2 hover:rotate-12">
-                    ⬇️
-                </a>
-                <Navigation />
+              
+            <div>
+            <Navigation />
             </div>
-            <hr className="mx-8" />
-            <Article />
+              
+            </div>
+          
         </>
     );
 }
