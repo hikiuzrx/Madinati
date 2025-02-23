e51a7a89070f36ebf0be013ba7f53a1f0abf95a6
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-02-23 23:16:31 +0100

    Update page.tsx

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,12 @@
+import Navigation from '@/components/Navigation'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Map
+    <div className='relative flex flex-col items-center justify-center lg:min-h-screen gap-8 p-4 pb-16'>
+     <div>
+            <Navigation />
+            </div>
     </div>
   )
 }
