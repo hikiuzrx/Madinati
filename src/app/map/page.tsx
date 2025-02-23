ac9094f1c0741d25b3bad5ab0866d0fae1755438
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-02-23 23:42:48 +0100

    fix navabr

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 const page = () => {
   return (
     <div className='relative flex flex-col items-center justify-center lg:min-h-screen gap-8 p-4 pb-16 w-full'>
-     <div>
+     
             <Navigation />
             </div>
-    </div>
+   
   )
 }
 
